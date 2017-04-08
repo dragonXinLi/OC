c9be024bc799e2a3b5e8f71f8981b8f07335d912
LLong <l916617054@163.com>
2017-04-08 18:09:13 +0800

    333

--- apple/apple/3rd/LeRrefreshControl.m
+++ apple/apple/3rd/LeRrefreshControl.m
@@ -62,7 +62,10 @@ static NSString *const kRefreshControlRefreshDone = @"kRefreshControlRefreshDone
         [self addSubview:activityView];
         
         arrowView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"refreshArrow"]];
-//        arrowView.center = CGPointMake(activityView.center.x, <#CGFloat y#>)
+        arrowView.center = CGPointMake(activityView.center.x, tips.center.y);
+        self.backgroundColor = [UIColor clearColor];
+        
+//        [self ]
     }
     return self;
 }
@@ -92,7 +95,76 @@ static NSString *const kRefreshControlRefreshDone = @"kRefreshControlRefreshDone
 }
 
 
+- (void)changeState:(NSString *)newState andValue:(id)value andForce:(BOOL)force
+{
+    NSArray *stateMachie = [NSArray arrayWithObjects:kRefreshControlIdle , kRefreshControlPulling,kRefreshControlRefreshing,kRefreshControlRefreshDone, nil];
+    if(state && force == NO)
+    {
+        if([stateMachie containsObject:newState])
+        {
+            NSUInteger old = [stateMachie indexOfObject:state];
+            NSUInteger new = [stateMachie indexOfObject:newState];
+            
+            if((old + 1) % stateMachie.count == new || old == new)
+            {
+                
+            }
+        }
+    }
+}
 
 
+- (void)changeStateEx:(NSString *)newState andValue:(id)value
+{
+    BOOL hasChanged = !(state == newState);
+    
+    if(newState)
+    {
+        state = newState;
+        if([state isEqualToString:kRefreshControlIdle] && hasChanged)
+        {
+            arrowView.transform = CGAffineTransformMakeRotation(0);
+            [activityView stopAnimating];
+        }else if([state isEqualToString:kRefreshControlPulling])
+        {
+            NSString *str1 = NSLocalizedStringFromTable(@"Pull down to refresh", @"Common", nil);
+            
+            CGAffineTransform t = CGAffineTransformMakeRotation(0);
+            if([value CGPointValue].y < -self.frame.size.height)
+            {
+                str1 = NSLocalizedStringFromTable(@"Loosen to refresh", @"Common", nil);
+                t = CGAffineTransformMakeRotation(M_PI);
+            }
+            
+            NSString *str = [NSString stringWithFormat:@"%@",str1];
+            if([tips.text isEqualToString:str] == NO)
+            {
+                [self setTipsString:str];
+            }
+            
+            arrowView.hidden = NO;
+            if(CGAffineTransformEqualToTransform(t, arrowView.transform) == NO)
+            {
+                [self setTipsString:str];
+                [UIView animateWithDuration:0.15 animations:^{
+                    arrowView.transform = t;
+                }];
+            }
+        }else if ([state isEqualToString:kRefreshControlRefreshing] && hasChanged)
+        {
+            NSString *str1 = NSLocalizedStringFromTable(@"Refreshing...", @"Common", nil);
+        }
+        
+    }
+}
 
+
+- (void)setTipsString:(NSString *)str
+{
+    CGSize strSize = [str sizeWithFont:tips.font constrainedToSize:self.frame.size];
+    strSize.height += tips.font.capHeight * 2;
+    
+    tips.text = str;
+    tips.frame = CGRectMake(0, (self.height - strSize.height)/2, tips.width, strSize.height);
+}
 @end
