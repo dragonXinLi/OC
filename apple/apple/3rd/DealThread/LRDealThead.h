54c34dcaa468beb3ee61b4f443ea5e266bc5ef67
LLong <l916617054@163.com>
2017-05-08 20:34:34 +0800

    计划第1天:写thread子线程类

---
+++ apple/apple/3rd/DealThread/LRDealThead.h
@@ -0,0 +1,13 @@
+//
+//  LRDealThead.h
+//  apple
+//
+//  Created by sangfor on 17/5/8.
+//  Copyright © 2017年 com.sangfor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LRDealThead : NSObject
+
+@end
---
+++ apple/apple/3rd/DealThread/LRDealThead.m
@@ -0,0 +1,13 @@
+//
+//  LRDealThead.m
+//  apple
+//
+//  Created by sangfor on 17/5/8.
+//  Copyright © 2017年 com.sangfor. All rights reserved.
+//
+
+#import "LRDealThead.h"
+
+@implementation LRDealThead
+
+@end
