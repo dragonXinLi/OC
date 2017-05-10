422330bcf4239c07305fceec79c219336211daab
LLong <l916617054@163.com>
2017-05-10 19:03:44 +0800

    asyncSocket文件更新

--- apple/apple/3rd/Net/AsyncTcpSocket.m
+++ apple/apple/3rd/Net/AsyncTcpSocket.m
@@ -21,13 +21,57 @@ typedef  enum : NSUInteger{
 static NSString *const AsyncTcpSocketQueueName = @"AsyncTcpSocketQueueName";
 static NSString *const kConnectTimerName = @"ConnectTimerName";
 
-#define kRecvBuffLength
+#define kRecvBuffLength 1024
 @interface AsyncTcpSocket()<NSStreamDelegate>
+@property (nonatomic , strong) NSMutableData *toSendData;
+@property (nonatomic , strong) NSMutableData *recvBuff;
+@property (nonatomic , strong) NSMutableDictionary *timers;
+@property (nonatomic , assign) eConnectState state;
 
+@property (nonatomic , weak) id<AsyncTcpSocketDelegate> delegate;
+@property (nonatomic , strong) LRDealThead *delegateDealThread;
+@property (nonatomic , strong) LRDealThead *socketDealThread;
 
+@property (nonatomic , strong) NSInputStream *inputStream;
+@property (nonatomic , strong) NSOutputStream *outputStream;
+
+@property (nonatomic , assign) BOOL readOpend;
+@property (nonatomic , assign) BOOL writeOpend;
+@property (nonatomic , assign) BOOL needReleaseDealThread;
 
 @end
 
 @implementation AsyncTcpSocket
+
+- (id)initWithDelegate:(id<AsyncTcpSocketDelegate>)delegate delegateDealThread:(LRDealThead *)dq socketDealThread:(LRDealThead *)sq
+{
+    self = [super init];
+    if(self)
+    {
+        self.toSendData = [NSMutableData dataWithCapacity:kRecvBuffLength];
+        self.timers = [NSMutableDictionary dictionaryWithCapacity:20];
+        
+        self.delegate = delegate;
+        
+        if (sq) {
+            self.socketDealThread = sq;
+        }else
+        {
+            self.socketDealThread = [[LRDealThead alloc] initWithName:AsyncTcpSocketQueueName];
+            [self.socketDealThread start];
+            self.needReleaseDealThread = YES;
+        }
+        
+        if (dq) {
+            self.delegateDealThread = dq;
+        }else
+        {
+            self.delegateDealThread = self.socketDealThread;
+        }
+    }
+    return self;
+}
+
+
 
 @end
